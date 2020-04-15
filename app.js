7f2a5da163fc2c9f607d9192254116980543cc07
Ola Ishola <olaishola@hotmail.co.uk>
2020-04-15 22:26:35 +0100

    UI and few tweaks on the app

--- app.js
+++ app.js
@@ -1,52 +1,54 @@
-window.onload = function () {
-    function startBtn() {
-        document.querySelector(".signUp").onclick = function () {
-            let user = document.querySelector("#userName").value;
-            let WelcomeNote = document.getElementsByClassName("welcome");
+function timer(time) {
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
 
-            WelcomeNote.innerHTML = `Welcome ${user} and your Game starts now`;
-        };
+    if (seconds < 10) {
+        seconds = `0${seconds}`;
     }
 
-    function timer(time) {
-        const minutes = Math.floor(time / 60);
-        let seconds = time % 60;
+    return `${minutes}:${seconds}`;
+}
 
-        if (seconds < 10) {
-            seconds = `0${seconds}`;
+const TIME_LIMIT = 120;
+
+let timeUsed = 0;
+let remainingTime = TIME_LIMIT;
+
+//resetting countdown
+function timesUp() {
+    clearInterval(timeInterval);
+}
+
+//setting the countdown
+
+let timeInterval = null;
+
+function startTimer() {
+    timeInterval = setInterval(() => {
+        timeUsed = timeUsed + 1;
+        remainingTime = TIME_LIMIT - timeUsed;
+
+        let display = (document.querySelector(".gameTime").innerHTML = "Duration: " + timer(remainingTime));
+        if (remainingTime === 60) {
+            alert("You have 1mins left, click OK to continue");
+            // display.style.color = "red";
         }
+        if (remainingTime === 0) {
+            alert("Your Time is Up");
+            timesUp();
+        }
+    }, 1000);
+}
+startTimer();
 
-        return `${minutes}:${seconds}`;
-    }
+// function startBtn() {
+//     document.querySelector(".signUp").onclick = function () {
+//         let user = document.querySelector("#userName").value;
+//         let WelcomeNote = document.querySelector("h1");
 
-    const TIME_LIMIT = 300;
-
-    let timeUsed = 0;
-    let remainingTime = TIME_LIMIT;
-
-    //resetting countdown
-    function timesUp() {
-        clearInterval(timeInterval);
-    }
-
-    //setting the countdown
-
-    let timeInterval = null;
-
-    function startTimer() {
-        timeInterval = setInterval(() => {
-            timeUsed = timeUsed + 1;
-            remainingTime = TIME_LIMIT - timeUsed;
-
-            document.querySelector(".gameTime").innerHTML = "Duration: " + timer(remainingTime);
-            if (remainingTime === 0) {
-                alert("Your time is Up");
-                timesUp();
-            }
-        }, 1000);
-    }
-    startTimer();
-};
+//         WelcomeNote.innerHTML = `Welcome ${user} and your Game starts now`;
+//     };
+// }
 
 // how to set countdown time in js when a btn is clicked
 //btn clicked when prev or next is clicked bring new question
