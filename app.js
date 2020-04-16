3918de02e91cd1687d6804109d764a0cd6103bca
Ola Ishola <olaishola@hotmail.co.uk>
2020-04-16 22:46:10 +0100

    app not working fully

--- app.js
+++ app.js
@@ -41,6 +41,114 @@ function startTimer() {
 }
 startTimer();
 
+let correct = 0;
+let wrong = 0;
+let numC;
+let result;
+let score;
+let msg;
+
+function randValue(maxValue) {
+    let day = new Date();
+    let hour = day.getHours();
+    let min = day.getMinutes();
+    let sec = day.getSeconds();
+    let miliSec = day.getTime();
+
+    return (hour * 3600 + min * 60 + sec + miliSec) % maxValue;
+}
+
+function random(maxValue) {
+    day = new Date();
+    let mil = day.getTime();
+    return `${mil % maxValue}`;
+}
+
+function addition() {
+    if (document.gameForm.arithmetic[0].checked) maxValue = 10;
+    else {
+        if (document.gameForm.arithmetic[1].checked) maxValue = 30;
+        else {
+            maxValue = 60;
+        }
+    }
+    let numA = randValue(maxValue);
+    let numB = random(maxValue);
+    numC = numA + numB;
+    alert(numC);
+    result = prompt(`${numA}` + "+" + `${numB}` + " = ", "");
+    answer();
+}
+
+function subtract() {
+    if (document.gameForm.arithmetic[0].checked) maxValue = 10;
+    else {
+        if (document.gameForm.arithmetic[1].checked) maxValue = 30;
+        else {
+            maxValue = 60;
+        }
+    }
+    let numA = randValue(maxValue);
+    let numB = random(maxValue);
+    numC = `${numA}` - `${numB}`;
+
+    result = prompt(`${numA}` + "-" + `${numB}` + " = ", 0);
+    answer();
+}
+
+function divide() {
+    if (document.gameForm.arithmetic[0].checked) maxValue = 10;
+    else {
+        if (document.gameForm.arithmetic[1].checked) maxValue = 30;
+        else {
+            maxValue = 60;
+        }
+    }
+    let numA = randValue(maxValue);
+    let numB = random(maxValue);
+    numC = `${numA}` / `${numB}`;
+    numC = Math.round(numC);
+    window.alert("Please round your answer off:\n" + ".5 or higher rounds one number up\n" + ".4 or lower rounds one number down");
+    result = prompt(numA + "/" + numB + " = ", 0);
+    answer();
+}
+
+function multiply() {
+    if (document.gameForm.arithmetic[0].checked) maxValue = 10;
+    else {
+        if (document.gameForm.arithmetic[1].checked) maxValue = 30;
+        else {
+            maxValue = 60;
+        }
+    }
+    let numA = randValue(maxValue);
+    let numB = random(maxValue);
+    numC = `${numA * numB}`;
+    result = prompt(numA + "*" + numB + " = ", 0);
+
+    answer();
+}
+function check() {
+    if (correct + wrong != 0) {
+        score = "" + (correct / (correct + wrong)) * 100;
+        score = score.substring(0, 4) + "%";
+        alert("YOUR SCORE: " + score + "\n" + correct + " correct\n" + wrong + " incorrect");
+    } else alert("You have not completed any exercise yet.");
+}
+
+function answer() {
+    if (result == numC) {
+        correct++;
+        msg = "Congrats, your answer is correct";
+    } else {
+        wrong++;
+        msg = "sorry " + result + " is incorrect.\n\n" + "The correct ans was " + numC + ".";
+    }
+    score = "" + (correct / (correct + wrong)) * 100;
+    score = score.substring(0, 4) + "%";
+    alert(msg + "\n\nYOUR SCORE:  " + score + "\n" + correct + " correct\n" + wrong + " incorrect");
+}
+
 // function startBtn() {
 //     document.querySelector(".signUp").onclick = function () {
 //         let user = document.querySelector("#userName").value;
